5e2e3833cead580538d160a013fb550ce5dc6974
Rednir <davran.dilshat@gmail.com>
2021-05-31 15:11:45 +0100

    add test project

---
+++ CliOptions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using CliOptions;
+
+namespace CliOptions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
